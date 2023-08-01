8405bb54002a810f0711c2c48d473e5af967c8ba
Jose Armas <armasjose606@gmail.com>
2023-07-31 20:39:57 -0400

    Add: ninjas controller

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  constructor(private readonly ninjasService: NinjasService) {}
+
+  @Get(':id')
+  getNinja(@Body('id') id: string) {
+    return this.ninjasService.getNinja(+id);
+  }
+
+  @Get()
+  getNinjas(@Query('weapon') weapon: string) {
+    return this.ninjasService.getNinjas(weapon);
+  }
+
+  @Post()
+  createNinja(@Body() createNinja: CreateNinjaDto) {
+    return this.ninjasService.createNinja(createNinja);
+  }
+
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinja: UpdateNinjaDto) {
+    return this.ninjasService.updateNinja(+id, updateNinja);
+  }
+
+  @Delete(':id')
+  deleteNinja(@Param('id') id: string) {
+    return this.ninjasService.removeNinja(+id);
+  }
+}
