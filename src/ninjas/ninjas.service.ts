6dc7c9350c5bdf62e4acf98cecf4fca4fdf8dd7b
Jose Armas <armasjose606@gmail.com>
2023-07-31 20:39:51 -0400

    Add: ninjas service

--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+  private ninjas = [
+    { id: 0, name: 'SwordMaster', weapon: 'Katana' },
+    { id: 1, name: 'NinjaStar', weapon: 'ThrowingStars' },
+  ];
+
+  getNinjas(weapon?: string) {
+    if (weapon) {
+      return this.ninjas.filter((ninja) => ninja.weapon === weapon);
+    }
+
+    return this.ninjas;
+  }
+
+  getNinja(id: number) {
+    const ninja = this.ninjas.find((ninja) => ninja.id === id);
+
+    if (!ninja) {
+      throw new Error('Ninja not found');
+    }
+
+    return ninja;
+  }
+
+  createNinja(createNinjaDto: CreateNinjaDto) {
+    this.ninjas.push(createNinjaDto);
+
+    return createNinjaDto;
+  }
+
+  updateNinja(id: number, updateNinjaDto: UpdateNinjaDto) {
+    this.ninjas = this.ninjas.map((ninja) => {
+      if (ninja.id === id) {
+        return { ...ninja, ...updateNinjaDto };
+      }
+
+      return ninja;
+    });
+
+    return this.getNinja(id);
+  }
+
+  removeNinja(id: number) {
+    const ninja = this.ninjas.find((ninja) => ninja.id !== id);
+
+    this.ninjas.filter((ninja) => ninja.id === id);
+
+    return ninja;
+  }
+}
